86cb9400d66d4451d6ecfad115321caaaeaa1c6b
KathyD <dedrickkj@svgs.k12.va.us>
2024-12-02 09:21:53 -0500

    uh

---
+++ basicserver/src/main/java/com/svgs/Animal.java
@@ -0,0 +1,9 @@
+package com.svgs;
+
+public class Animal {
+    public String name;
+    public String[] locations;
+    public String most_distinctive_feature;
+    public String diet;
+    public String slogan;
+}
